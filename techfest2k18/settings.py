b92acf22444dd88eedcf6cb807816b5c5388f352
Prakhar Varshney <prakhar_varshney@outlook.com>
2022-03-04 19:14:04 -0500

    Update settings.py

--- techfest2k18/settings.py
+++ techfest2k18/settings.py
@@ -23,10 +23,10 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(pl^&i!^x-=d*z62ky+=&w5=)3hf440etry5wyhxzbu268et=^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
